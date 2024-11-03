b61654fa160659cc9b2c607ab3cfec37a5132dbe
Aarav1433 <aarav.kp.patel@gmail.com>
2024-11-03 18:59:14 -0500

    Add files via upload

---
+++ Presidents.py
@@ -0,0 +1,687 @@
+Washington = {
+  "Domestic" : {
+    "5" : 9,
+    "4" : 6,
+    "3" : 2,
+    "2" : 0,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 3,
+    "4" : 7,
+    "3" : 2,
+    "2" : 3,
+    "1" : 1,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 15,
+    "2" : 2,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 16,
+    "1" : 1,
+    "0" : 0,
+  },
+  "name" : "Washington"
+}
+
+Lincoln = {
+  "Domestic" : {
+    "5" : 10,
+    "4" : 7,
+    "3" : 0,
+    "2" : 0,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 1,
+    "4" : 5,
+    "3" : 8,
+    "2" : 1,
+    "1" : 1,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 16,
+    "2" : 1,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 14,
+    "1" : 3,
+    "0" : 0,
+  },
+  "name" : "Lincoln"
+}
+
+FDR = {
+  "Domestic" : {
+    "5" : 7,
+    "4" : 9,
+    "3" : 1,
+    "2" : 0,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 2,
+    "4" : 6,
+    "3" : 7,
+    "2" : 2,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 8,
+    "2" : 9,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 8,
+    "1" : 9,
+    "0" : 0,
+  },
+  "name" : "FDR"
+}
+
+JFK = {
+  "Domestic" : {
+    "5" : 2,
+    "4" : 8,
+    "3" : 4,
+    "2" : 3,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 1,
+    "4" : 5,
+    "3" : 7,
+    "2" : 3,
+    "1" : 1,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 12,
+    "2" : 3,
+    "1" : 1,
+    "0" : 1,
+  },
+  "Aura" : {
+    "2" : 12,
+    "1" : 4,
+    "0" : 1,
+  },
+  "name" : "JFK"
+}
+
+TR = {
+  "Domestic" : {
+    "5" : 5,
+    "4" : 8,
+    "3" : 3,
+    "2" : 0,
+    "1" : 1,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 2,
+    "4" : 1,
+    "3" : 8,
+    "2" : 4,
+    "1" : 2,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 11,
+    "2" : 5,
+    "1" : 1,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 11,
+    "1" : 5,
+    "0" : 1,
+  },
+  "name" : "TR"
+}
+
+Eisenhower = {
+  "Domestic" : {
+    "5" : 2,
+    "4" : 3,
+    "3" : 8,
+    "2" : 2,
+    "1" : 0,
+    "0" : 2,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 7,
+    "3" : 6,
+    "2" : 3,
+    "1" : 0,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 1,
+    "2" : 8,
+    "1" : 7,
+    "0" : 1,
+  },
+  "Aura" : {
+    "2" : 2,
+    "1" : 9,
+    "0" : 6,
+  },
+  "name" : "Eisenhower"
+}
+
+Monroe = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 2,
+    "3" : 8,
+    "2" : 4,
+    "1" : 3,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 3,
+    "4" : 4,
+    "3" : 4,
+    "2" : 4,
+    "1" : 2,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 4,
+    "2" : 6,
+    "1" : 7,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 4,
+    "1" : 7,
+    "0" : 6,
+  },
+  "name" : "Monroe"
+}
+
+Truman = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 4,
+    "3" : 10,
+    "2" : 3,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 3,
+    "4" : 4,
+    "3" : 6,
+    "2" : 2,
+    "1" : 2,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 4,
+    "2" : 4,
+    "1" : 7,
+    "0" : 2,
+  },
+  "Aura" : {
+    "2" : 3,
+    "1" : 9,
+    "0" : 5,
+  },
+  "name" : "Truman"
+}
+
+Johnson = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 1,
+    "2" : 2,
+    "1" : 3,
+    "0" : 10,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 2,
+    "2" : 2,
+    "1" : 3,
+    "0" : 9,
+  },
+  "Legacy" : {
+    "3" : 0,
+    "2" : 1,
+    "1" : 5,
+    "0" : 11,
+  },
+  "Aura" : {
+    "2" : 2,
+    "1" : 1,
+    "0" : 14,
+  },
+  "name" : "Johnson"
+}
+
+Jefferson = {
+  "Domestic" : {
+    "5" : 1,
+    "4" : 5,
+    "3" : 3,
+    "2" : 5,
+    "1" : 1,
+    "0" : 2,
+  },
+  "Foreign" : {
+    "5" : 2,
+    "4" : 6,
+    "3" : 1,
+    "2" : 4,
+    "1" : 1,
+    "0" : 3,
+  },
+  "Legacy" : {
+    "3" : 6,
+    "2" : 4,
+    "1" : 6,
+    "0" : 1,
+  },
+  "Aura" : {
+    "2" : 7,
+    "1" : 7,
+    "0" : 3,
+  },
+  "name" : "Jefferson"
+}
+
+Reagan = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 5,
+    "3" : 3,
+    "2" : 3,
+    "1" : 3,
+    "0" : 3,
+  },
+  "Foreign" : {
+    "5" : 2,
+    "4" : 3,
+    "3" : 5,
+    "2" : 3,
+    "1" : 2,
+    "0" : 2,
+  },
+  "Legacy" : {
+    "3" : 4,
+    "2" : 7,
+    "1" : 5,
+    "0" : 1,
+  },
+  "Aura" : {
+    "2" : 10,
+    "1" : 4,
+    "0" : 3,
+  },
+  "name" : "Reagan"
+}
+
+Wilson = {
+  "Domestic" : {
+    "5" : 2,
+    "4" : 4,
+    "3" : 7,
+    "2" : 3,
+    "1" : 0,
+    "0" : 1,
+  },
+  "Foreign" : {
+    "5" : 4,
+    "4" : 5,
+    "3" : 7,
+    "2" : 1,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 3,
+    "2" : 12,
+    "1" : 2,
+    "0" : 0,
+  },
+  "Aura" : {
+    "2" : 5,
+    "1" : 8,
+    "0" : 4,
+  },
+  "name" : "Wilson"
+}
+
+Madison = {
+  "Domestic" : {
+    "5" : 2,
+    "4" : 5,
+    "3" : 6,
+    "2" : 3,
+    "1" : 1,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 6,
+    "2" : 7,
+    "1" : 2,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 1,
+    "2" : 7,
+    "1" : 6,
+    "0" : 3,
+  },
+  "Aura" : {
+    "2" : 4,
+    "1" : 7,
+    "0" : 6,
+  },
+  "name" : "Madison"
+}
+
+Jackson = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 0,
+    "2" : 1,
+    "1" : 8,
+    "0" : 7,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 0,
+    "2" : 7,
+    "1" : 3,
+    "0" : 6,
+  },
+  "Legacy" : {
+    "3" : 4,
+    "2" : 3,
+    "1" : 6,
+    "0" : 4,
+  },
+  "Aura" : {
+    "2" : 10,
+    "1" : 2,
+    "0" : 5,
+  },
+  "name" : "Jackson"
+}
+
+Cleveland = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 4,
+    "2" : 9,
+    "1" : 2,
+    "0" : 1,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 5,
+    "2" : 7,
+    "1" : 3,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 0,
+    "2" : 2,
+    "1" : 5,
+    "0" : 10,
+  },
+  "Aura" : {
+    "2" : 0,
+    "1" : 6,
+    "0" : 11,
+  },
+  "name" : "Cleveland"
+}
+
+Carter = {
+  "Domestic" : {
+    "5" : 1,
+    "4" : 2,
+    "3" : 11,
+    "2" : 3,
+    "1" : 0,
+    "0" : 0,
+  },
+  "Foreign" : {
+    "5" : 2,
+    "4" : 4,
+    "3" : 5,
+    "2" : 4,
+    "1" : 2,
+    "0" : 0,
+  },
+  "Legacy" : {
+    "3" : 0,
+    "2" : 6,
+    "1" : 8,
+    "0" : 3,
+  },
+  "Aura" : {
+    "2" : 4,
+    "1" : 9,
+    "0" : 4,
+  },
+  "name" : "Carter"
+}
+
+Nixon = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 2,
+    "3" : 7,
+    "2" : 5,
+    "1" : 2,
+    "0" : 2,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 7,
+    "3" : 7,
+    "2" : 0,
+    "1" : 2,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 3,
+    "2" : 4,
+    "1" : 4,
+    "0" : 6,
+  },
+  "Aura" : {
+    "2" : 4,
+    "1" : 9,
+    "0" : 4,
+  },
+  "name" : "Nixon"
+}
+
+Hoover = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 6,
+    "2" : 4,
+    "1" : 5,
+    "0" : 1,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 2,
+    "2" : 8,
+    "1" : 5,
+    "0" : 1,
+  },
+  "Legacy" : {
+    "3" : 1,
+    "2" : 3,
+    "1" : 7,
+    "0" : 6,
+  },
+  "Aura" : {
+    "2" : 2,
+    "1" : 4,
+    "0" : 11,
+  },
+  "name" : "Hoover"
+}
+
+Polk = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 1,
+    "3" : 8,
+    "2" : 3,
+    "1" : 2,
+    "0" : 3,
+  },
+  "Foreign" : {
+    "5" : 1,
+    "4" : 2,
+    "3" : 5,
+    "2" : 4,
+    "1" : 2,
+    "0" : 3,
+  },
+  "Legacy" : {
+    "3" : 1,
+    "2" : 3,
+    "1" : 7,
+    "0" : 6,
+  },
+  "Aura" : {
+    "2" : 4,
+    "1" : 5,
+    "0" : 8,
+  },
+  "name" : "Polk"
+}
+
+Bush = {
+  "Domestic" : {
+    "5" : 0,
+    "4" : 3,
+    "3" : 3,
+    "2" : 6,
+    "1" : 3,
+    "0" : 2,
+  },
+  "Foreign" : {
+    "5" : 0,
+    "4" : 3,
+    "3" : 2,
+    "2" : 3,
+    "1" : 5,
+    "0" : 4,
+  },
+  "Legacy" : {
+    "3" : 1,
+    "2" : 7,
+    "1" : 4,
+    "0" : 5,
+  },
+  "Aura" : {
+    "2" : 2,
+    "1" : 8,
+    "0" : 7,
+  },
+  "name" : "Bush"
+}
+
+Presidents = [Washington, Lincoln, FDR, JFK, TR, Eisenhower, Monroe, Truman, Johnson, Jefferson, Reagan, Wilson, Madison, Jackson, Cleveland, Carter, Nixon, Hoover, Polk, Bush]
+
+def getComplexityScore(PresidentDict):
+    score = 0
+    if PresidentDict["Domestic"]["5"] >= 3 and PresidentDict["Domestic"]["4"] >= 3 and PresidentDict["Domestic"]["3"] >= 3 and PresidentDict["Domestic"]["2"] >= 3 and PresidentDict["Domestic"]["1"] >= 3 and PresidentDict["Domestic"]["0"] >=3:
+        score += 20
+    elif PresidentDict["Domestic"]["5"] >= 2 and PresidentDict["Domestic"]["4"] >= 2 and PresidentDict["Domestic"]["3"] >= 2 and PresidentDict["Domestic"]["2"] >= 2 and PresidentDict["Domestic"]["1"] >= 2 and PresidentDict["Domestic"]["0"] >=2:
+        score += 12
+    elif PresidentDict["Domestic"]["5"] >= 1 and PresidentDict["Domestic"]["4"] >= 1 and PresidentDict["Domestic"]["3"] >= 1 and PresidentDict["Domestic"]["2"] >= 1 and PresidentDict["Domestic"]["1"] >= 1 and PresidentDict["Domestic"]["0"] >= 1:
+        score += 5
+    else:
+        score += 0
+        
+    if PresidentDict["Foreign"]["5"] >= 3 and PresidentDict["Foreign"]["4"] >= 3 and PresidentDict["Foreign"]["3"] >= 3 and PresidentDict["Foreign"]["2"] >= 3 and PresidentDict["Foreign"]["1"] >= 3 and PresidentDict["Foreign"]["0"] >=3:
+        score += 20
+    elif PresidentDict["Foreign"]["5"] >= 2 and PresidentDict["Foreign"]["4"] >= 2 and PresidentDict["Foreign"]["3"] >= 2 and PresidentDict["Foreign"]["2"] >= 2 and PresidentDict["Foreign"]["1"] >= 2 and PresidentDict["Foreign"]["0"] >=2:
+        score += 12
+    elif PresidentDict["Foreign"]["5"] >= 1 and PresidentDict["Foreign"]["4"] >= 1 and PresidentDict["Foreign"]["3"] >= 1 and PresidentDict["Foreign"]["2"] >= 1 and PresidentDict["Foreign"]["1"] >= 1 and PresidentDict["Foreign"]["0"] >= 1:
+        score += 5
+    else:
+        score += 0
+        
+    if PresidentDict["Legacy"]["3"] >= 4 and PresidentDict["Legacy"]["2"] >= 4 and PresidentDict["Legacy"]["1"] >= 4 and PresidentDict["Legacy"]["0"] >=4:
+        score += 30
+    elif PresidentDict["Legacy"]["3"] >= 3 and PresidentDict["Legacy"]["2"] >= 3 and PresidentDict["Legacy"]["1"] >= 3 and PresidentDict["Legacy"]["0"] >=3:
+        score += 15
+    elif PresidentDict["Legacy"]["3"] >= 2 and PresidentDict["Legacy"]["2"] >= 2 and PresidentDict["Legacy"]["1"] >= 2 and PresidentDict["Legacy"]["0"] >= 2:
+        score += 10
+    else:
+        score += 0
+    
+    if PresidentDict["Aura"]["2"] >= 6 and PresidentDict["Aura"]["1"] >= 6 and PresidentDict["Aura"]["0"] >=6:
+        score += 40
+    elif PresidentDict["Aura"]["2"] >= 5 and PresidentDict["Aura"]["1"] >= 5 and PresidentDict["Aura"]["0"] >=5:
+        score += 30
+    elif PresidentDict["Aura"]["2"] >= 4 and PresidentDict["Aura"]["1"] >= 4 and PresidentDict["Aura"]["0"] >=4:
+        score += 20
+    elif PresidentDict["Aura"]["2"] >= 3 and PresidentDict["Aura"]["1"] >= 3 and PresidentDict["Aura"]["0"] >=3:
+        score += 10
+    elif PresidentDict["Aura"]["2"] >= 2 and PresidentDict["Aura"]["1"] >= 2 and PresidentDict["Aura"]["0"] >= 2:
+        score += 8
+    elif PresidentDict["Aura"]["2"] >= 1 and PresidentDict["Aura"]["1"] >= 1 and PresidentDict["Aura"]["0"] >=1:
+        score += 5
+    else:
+        score += 0
+        
+    return score
+
+def getScore(PresidentDict):
+    domScore = ((PresidentDict["Domestic"]["5"]*5) +(PresidentDict["Domestic"]["4"]*4)+(PresidentDict["Domestic"]["3"]*3) +(PresidentDict["Domestic"]["2"]*2) + (PresidentDict["Domestic"]["1"]*1) + (PresidentDict["Domestic"]["0"]*0))/17
+    domScore = int(domScore+0.5)
+    forScore = ((PresidentDict["Foreign"]["5"]*5) +(PresidentDict["Foreign"]["4"]*4)+(PresidentDict["Foreign"]["3"]*3) +(PresidentDict["Foreign"]["2"]*2) + (PresidentDict["Foreign"]["1"]*1) + (PresidentDict["Foreign"]["0"]*0))/17
+    forScore = int(forScore+0.5)
+    legScore = ((PresidentDict["Legacy"]["3"]*3) +(PresidentDict["Legacy"]["2"]*2) + (PresidentDict["Legacy"]["1"]*1) + (PresidentDict["Legacy"]["0"]*0))/17
+    legScore = int(legScore+0.5)
+    aurScore = ((PresidentDict["Aura"]["2"]*2) + (PresidentDict["Aura"]["1"]*1) + (PresidentDict["Aura"]["0"]*0))/17
+    aurScore = int(aurScore+0.5) 
+    totScore = domScore + forScore + legScore + aurScore
+    return totScore
+for i in Presidents:
+    print(i["name"])
+    print(getScore(i))
+   
+    
+
+    
+
